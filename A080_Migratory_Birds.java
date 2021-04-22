4f766e47127d2545f26796144fb306ebfbea097e
Alejandro <alejandro.omar.rizzuto@gmail.com>
2021-04-22 15:22:50 -0300

    Adding more Challenges

--- A080_Migratory_Birds.java
+++ A080_Migratory_Birds.java
@@ -2,6 +2,7 @@
 
 import java.io.*;
 import java.util.*;
+import java.util.Map.Entry;
 import java.util.stream.*;
 import static java.util.stream.Collectors.toList;
 
@@ -9,13 +10,25 @@ public class A080_Migratory_Birds {
 
     // Complete the migratoryBirds function below.
     static int migratoryBirds(List<Integer> arr) {
+        Map<Integer, Integer> map = new HashMap<>();
 
+        for (Integer t : arr) {
+            Integer val = map.get(t);
+            map.put(t, val == null ? 1 : val + 1);
+        }
 
+        Entry<Integer,Integer> max = null;
+
+        for (Entry<Integer, Integer> e : map.entrySet()) {
+            if (max == null || e.getValue() > max.getValue()) max = e;
+        }
+
+        return max.getKey();
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
 
