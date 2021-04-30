b3d7fb0a75630010d6258714ecc06510bfcb6264
Alejandro <alejandro.omar.rizzuto@gmail.com>
2021-04-30 17:54:28 -0300

    Hackerrank 93 and 94

--- Exercises/A093_Utopian_Tree.java
+++ Exercises/A093_Utopian_Tree.java
@@ -1,36 +1,25 @@
 package Exercises;
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
 import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
 class Result93 {
 
     public static int utopianTree(int n) {
-        int total = 0;
+        int total = 1;
         return utopianTreeRecursive(n, total);
     }
 
     private static int utopianTreeRecursive(int n, int total) {
-        return 0;
+        if (n == 0) return total;
+        return utopianTreeRecursive(n-1, total%2==0?total+1:total*2);
     }
-
-    
-
 }
 
 public class A093_Utopian_Tree {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int t = Integer.parseInt(bufferedReader.readLine().trim());
 
